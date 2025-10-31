4ba74a0065e7104a7fe679e3ca8465b298160e9f
jjawar001 <jjawar001@gmail.com>
2025-10-31 14:24:45 -0400

    Update spells.js with auto rendering

--- spells.js
+++ spells.js
@@ -15,3 +15,48 @@ hamburgerBtn.addEventListener('click', ()=>{
     hamburgerBtn.innerHTML = `<img src="./images/hamburger-menu.svg" alt="hamburger menu icon">`
   }
 })
+
+
+const tableBody = document.getElementById('table-body');
+const spellListTable = document.getElementById('spell-list-table');
+const spellDescription = document.getElementById('spell-description');
+
+
+function renderSpellTable(spellData) {
+  let newHTML = ''
+  spellData.forEach(spell => {
+      newHTML += `<tr id="${spell.id}">
+                <td>${spell.name}</td>
+                <td>${spell.level}</td>
+                <td>${spell.school}</td>
+                <td>${spell.components}</td>
+              </tr>`
+  })
+
+  tableBody.insertAdjacentHTML('beforeEnd', newHTML)
+}
+
+renderSpellTable(catSpells)
+
+spellListTable.addEventListener('click', function(e) {
+  const row = e.target.closest('tr')
+  if (row) {
+    catSpells.forEach((spell) => {
+      if (row.id === spell.id) {
+        spellDescription.innerHTML = ''
+        const spellDescriptionText = `
+            <h2 class="medieval-font center">SPELL DESCRIPTION</h2>
+            <h3 class="bold center">${spell.name}</h3>
+            <p><span class="bold">Level:</span> ${spell.level}</p>
+            <p><span class="bold">School:</span> ${spell.school}</p>
+            <p><span class="bold">Casting Time:</span> ${spell.castingTime}</p>
+            <p><span class="bold">Range:</span> ${spell.range}</p>
+            <p><span class="bold">Duration:</span> ${spell.duration}</p>
+            <p><span class="bold">Components:</span> ${spell.components}</p>
+            <p><span class="bold">Description:</span> ${spell.description}</p>
+            <p><span class="bold">Effect:</span>${spell.effect}.</p>`
+        spellDescription.insertAdjacentHTML('beforeEnd', spellDescriptionText)
+      }
+    })
+  }
+})
